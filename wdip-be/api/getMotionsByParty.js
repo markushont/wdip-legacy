979661dd4662437888102740b32d8ce15d5f96d0
Yvonne Le <yvonne@hejare.se>
2018-10-19 15:30:18 +0200

    Used await instead of nested promises. Moved DEFAULT_parties to environment.json

--- wdip-be/api/getMotionsByParty.js
+++ wdip-be/api/getMotionsByParty.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const dbClient = require('../dbclient');
-const { WDIP_MOTION_INDEX } = require('../config/config');
-const DEFAULT_PARTIES = ["s", "m", "l", "kd", "v", "sd", "c", "mp", "nyd"];
+const { WDIP_MOTION_INDEX, WDIP_DEFAULT_PARTIES } = require('../config/config');
+
 const logger = require('../logger');
 
 async function getMotions(fromDateStrOverride = null, toDateStrOverride = null, outcome, parties) {
@@ -46,18 +46,18 @@ async function getMotions(fromDateStrOverride = null, toDateStrOverride = null, 
   }
 }
 
-module.exports = async function getMotionsByParty(fromDateStrOverride = new Date(2000, 0, 1), toDateStrOverride = new Date(2018, 0, 1), parties = DEFAULT_PARTIES) {
+module.exports = async function getMotionsByParty(fromDateStrOverride = new Date(2000, 0, 1), toDateStrOverride = new Date(2018, 0, 1), parties = WDIP_DEFAULT_PARTIES) {
   const posResponse = getMotions(fromDateStrOverride, toDateStrOverride, "bifall", parties);
   const negResponse = getMotions(fromDateStrOverride, toDateStrOverride, "avslag", parties);
-  return Promise.all([posResponse, negResponse]).then(function (responseQueue) {
-    var i = 0;
-    var partyData = [];
-    parties.forEach(function (party) {
-      const approved = responseQueue[0].responses[i].hits.total;
-      const declined = responseQueue[1].responses[i].hits.total;
-      partyData.push({ party: party, submitted: approved + declined, approved: approved, declined: declined });
-      i++;
-    });
-    return { fromDate: fromDateStrOverride, toDate: toDateStrOverride, results: partyData };
+  const responseQueue= await Promise.all([posResponse, negResponse]);
+  var i = 0;
+  var partyData = [];
+  parties.forEach(function (party) {
+    const approved = responseQueue[0].responses[i].hits.total;
+    const declined = responseQueue[1].responses[i].hits.total;
+    partyData.push({ party: party, submitted: approved + declined, approved: approved, declined: declined });
+    i++;
   });
+  return { fromDate: fromDateStrOverride, toDate: toDateStrOverride, results: partyData };
+
 };
