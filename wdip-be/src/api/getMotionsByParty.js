a62314bb5e86694b41bb637ba1078f9964f9833b
Henrik Svensson <henrik@hejare.se>
2018-10-20 22:58:30 +0200

    adds tslint

--- wdip-be/src/api/getMotionsByParty.js
+++ wdip-be/src/api/getMotionsByParty.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
-const dbClient = require('../dbclient');
-const { WDIP_MOTION_INDEX, WDIP_DEFAULT_PARTIES } = require('../config/config');
+const dbClient = require("../dbclient");
+const { WDIP_MOTION_INDEX, WDIP_DEFAULT_PARTIES } = require("../config/config");
 
-const logger = require('../logger');
+const logger = require("../logger");
 
 async function getMotions(fromDateStrOverride = null, toDateStrOverride = null, outcome, parties) {
   var toAddDocs = [];
-  parties.forEach(function (party) {
+  parties.forEach((party) => {
     toAddDocs.push({
       index: WDIP_MOTION_INDEX,
-      type: '_doc',
-      size: '0'
+      type: "_doc",
+      size: "0"
     });
     toAddDocs.push(
       {
@@ -46,13 +46,16 @@ async function getMotions(fromDateStrOverride = null, toDateStrOverride = null, 
   }
 }
 
-module.exports = async function getMotionsByParty(fromDateStrOverride = new Date(2000, 0, 1), toDateStrOverride = new Date(2018, 0, 1), parties = WDIP_DEFAULT_PARTIES) {
+async function getMotionsByParty(
+  fromDateStrOverride = new Date(2000, 0, 1),
+  toDateStrOverride = new Date(2018, 0, 1),
+  parties = WDIP_DEFAULT_PARTIES) {
   const posResponse = getMotions(fromDateStrOverride, toDateStrOverride, "bifall", parties);
   const negResponse = getMotions(fromDateStrOverride, toDateStrOverride, "avslag", parties);
-  const responseQueue= await Promise.all([posResponse, negResponse]);
+  const responseQueue = await Promise.all([posResponse, negResponse]);
   var i = 0;
   var partyData = [];
-  parties.forEach(function (party) {
+  parties.forEach((party) => {
     const approved = responseQueue[0].responses[i].hits.total;
     const declined = responseQueue[1].responses[i].hits.total;
     partyData.push({ party: party, submitted: approved + declined, approved: approved, declined: declined });
@@ -60,4 +63,6 @@ module.exports = async function getMotionsByParty(fromDateStrOverride = new Date
   });
   return { fromDate: fromDateStrOverride, toDate: toDateStrOverride, results: partyData };
 
-};
+}
+
+module.exports = getMotionsByParty;
