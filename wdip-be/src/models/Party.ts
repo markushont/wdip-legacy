3e4790e6e76cff029019f430bceb77426fa68282
Henrik Svensson <henrik@hejare.se>
2018-12-18 13:40:46 +0100

    adds folkpartiet and renames centern

--- wdip-be/src/models/Party.ts
+++ wdip-be/src/models/Party.ts
@@ -7,10 +7,11 @@ export interface Party {
 export const socialdemokraterna: Party = { id: "s", name: "Socialdemokraterna", abbreviation: "S" };
 export const moderaterna: Party = { id: "m", name: "Moderaterna", abbreviation: "M" };
 export const liberalerna: Party = { id: "l", name: "Liberalerna", abbreviation: "L" };
+export const folkpartiet: Party = { id: "fp", name: "Folkpartiet", abbreviation: "FP" };
 export const kristdemokraterna: Party = { id: "kd", name: "Kristdemokraterna", abbreviation: "KD" };
 export const vansterpartiet: Party = { id: "v", name: "Vänsterpartiet", abbreviation: "V" };
 export const sverigedemokraterna: Party = { id: "sd", name: "Sverigedemokraterna", abbreviation: "SD" };
-export const centern: Party = { id: "c", name: "Centern", abbreviation: "C" };
+export const centerpartiet: Party = { id: "c", name: "Centerpartiet", abbreviation: "C" };
 export const miljopartiet: Party = { id: "mp", name: "Miljöpartiet", abbreviation: "MP" };
 export const other: Party = { id: "ö", name: "Övriga", abbreviation: "Ö" };
 
@@ -18,11 +19,12 @@ export const parties =
     [
         socialdemokraterna,
         moderaterna,
+        folkpartiet,
         liberalerna,
         kristdemokraterna,
         vansterpartiet,
         sverigedemokraterna,
-        centern,
+        centerpartiet,
         miljopartiet
     ];
 
