1825e7f38050d5fd22604f9dec3437b0dbdbf1fe
Markus Silberstein Hont <markus@silberstein.nu>
2018-12-20 12:35:09 +0100

    removes references to deleted files

--- wdip-be/src/scheduledJobsHandler.js
+++ wdip-be/src/scheduledJobsHandler.js
@@ -1,20 +1,6 @@
 "use strict";
 
-const fetchMotions = require("./scheduledJobs/fetchMotions");
-const refreshPendingMotions = require("./scheduledJobs/refreshPendingMotions");
 import { importQueueStatus } from "./scheduledJobs/ImportQueueStatus";
-
-module.exports.fetchMotions = (event, context, callback) => {
-  let payload = event.body !== undefined ? JSON.parse(event.body) : undefined;
-
-  var from = payload && payload.from !== undefined ? payload.from : null;
-  var to = payload && payload.to !== undefined ? payload.to : null;
-  fetchMotions(from, to, callback);
-};
-
-module.exports.refreshPendingMotions = (event, context, callback) => {
-  refreshPendingMotions(callback);
-};
 
 module.exports.queueStatus = (event, context) => {
   importQueueStatus.logStatus();
