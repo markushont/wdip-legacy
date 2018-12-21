95581ee8f52d1234fb829c2da6f7964ba72a04d4
Markus Silberstein Hont <markus@silberstein.nu>
2018-12-21 14:03:46 +0100

    refactors getMotionsByParty and motions to comply with tslint and use proper imports

--- wdip-be/src/api/getMotionsByParty.ts
+++ wdip-be/src/api/getMotionsByParty.ts
@@ -1,24 +1,18 @@
 "use strict";
 
-const dbClient = require("../dbclient");
-const {
-  WDIP_MOTION_INDEX
-} = require("../config/config");
+import config from "../config/config";
+import dbClient from "../dbclient";
+import logger from "../logger";
+import { DocumentStatus as status } from "../models/DocumentStatus";
 
-const {
-  WDIP_DEFAULT_PARTIES,
-  WDIP_ACCEPTED,
-  WDIP_REJECTED,
-  WDIP_PARTIALLY_ACCEPTED
-} = require("../config/constants");
-
-const logger = require("../logger");
-
-async function getMotions(fromDateStrOverride = null, toDateStrOverride = null, outcome, parties) {
-  var toAddDocs = [];
+async function getMotions(outcome: string,
+                          parties: string[],
+                          fromDateStrOverride?: string,
+                          toDateStrOverride?: string) {
+  const toAddDocs = [];
   parties.forEach((party) => {
     toAddDocs.push({
-      index: WDIP_MOTION_INDEX,
+      index: config.WDIP_MOTION_INDEX,
       type: "_doc",
       size: "0"
     });
@@ -31,13 +25,13 @@ async function getMotions(fromDateStrOverride = null, toDateStrOverride = null, 
                 term: { "intressent.partibet": party }
               },
               {
-                term: { "status": outcome }
+                term: { status: outcome }
               },
               {
                 range: {
-                  "dateStr": {
-                    "gte": fromDateStrOverride,
-                    "lte": toDateStrOverride
+                  dateStr: {
+                    gte: fromDateStrOverride,
+                    lte: toDateStrOverride
                   }
                 }
               }
@@ -55,29 +49,28 @@ async function getMotions(fromDateStrOverride = null, toDateStrOverride = null, 
   }
 }
 
-async function getMotionsByParty(
-  fromDateStrOverride,
-  toDateStrOverride) {
+async function getMotionsByParty(fromDateStrOverride?: string, toDateStrOverride?: string) {
 
-  var parties = WDIP_DEFAULT_PARTIES;
+  const parties = config.WDIP_DEFAULT_PARTIES;
 
-  const posResponse = getMotions(fromDateStrOverride, toDateStrOverride, WDIP_ACCEPTED, parties);
-  const negResponse = getMotions(fromDateStrOverride, toDateStrOverride, WDIP_REJECTED, parties);
+  const posResponse = getMotions(status.APPROVED, parties, fromDateStrOverride, toDateStrOverride);
+  const negResponse = getMotions(status.REJECTED, parties, fromDateStrOverride, toDateStrOverride);
   const partiallyAcceptedResponse =
-    getMotions(fromDateStrOverride, toDateStrOverride, WDIP_PARTIALLY_ACCEPTED, parties);
+    getMotions(status.APPROVED_PARTIALLY, parties, fromDateStrOverride, toDateStrOverride);
   const responseQueue = await Promise.all([posResponse, negResponse, partiallyAcceptedResponse]);
-  var i = 0;
-  var partyData = [];
+
+  let i = 0;
+  const partyData = [];
   parties.forEach((party) => {
     const approved = responseQueue[0].responses[i].hits.total;
     const declined = responseQueue[1].responses[i].hits.total;
     const partiallyAccepted = responseQueue[2].responses[i].hits.total;
     partyData.push({
-      party: party,
+      party,
       submitted: approved + declined + partiallyAccepted,
-      approved: approved,
-      declined: declined,
-      partiallyAccepted: partiallyAccepted
+      approved,
+      declined,
+      partiallyAccepted
     });
     i++;
   });
