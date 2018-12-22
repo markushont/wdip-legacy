7b876ba0b530a030929e7e471a38eb3fb33838ec
Markus Silberstein Hont <markus@silberstein.nu>
2018-12-22 15:21:06 +0100

    adds unit tests for PROPOSITION and document references

--- wdip-be/src/models/__tests__/MotionDocument.test.ts
+++ wdip-be/src/models/__tests__/MotionDocument.test.ts
@@ -12,12 +12,17 @@ const expectedDocument1: MotionDocument = loadJsonFile("src/models/__mocks__/exp
 const sourceDocument2: any = loadJsonFile("src/models/__mocks__/motionDocument2.json");
 const expectedDocument2: MotionDocument = loadJsonFile("src/models/__mocks__/expectedDocument2.json");
 
+const sourceDocument3: any = loadJsonFile("src/models/__mocks__/motionDocument3.json");
+const expectedDocument3: MotionDocument = loadJsonFile("src/models/__mocks__/expectedDocument3.json");
+
 // Seems hard to compare instances of moments. This workaround removes them and the test
 // checks for the existance, which at least is something. TODO: Make better.
 delete expectedDocument1.meta;
 delete expectedDocument1.published;
 delete expectedDocument2.meta;
 delete expectedDocument2.published;
+delete expectedDocument3.meta;
+delete expectedDocument3.published;
 
 describe("MotionDocument tests", () => {
 
@@ -32,6 +37,14 @@ describe("MotionDocument tests", () => {
     test("Incomplete document", () => {
         const doc = transformMotionDocument(sourceDocument2.dokumentstatus);
         expect(doc).toMatchObject(expectedDocument2);
+        expect(isMoment(doc.published)).toBeTrue();
+        expect(isMoment(doc.meta.created)).toBeTrue();
+        expect(isMoment(doc.meta.updated)).toBeTrue();
+    });
+
+    test("Correctly transformed proposition", () => {
+        const doc = transformMotionDocument(sourceDocument3.dokumentstatus);
+        expect(doc).toMatchObject(expectedDocument3);
         expect(isMoment(doc.published)).toBeTrue();
         expect(isMoment(doc.meta.created)).toBeTrue();
         expect(isMoment(doc.meta.updated)).toBeTrue();
