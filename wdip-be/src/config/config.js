18b366b4827a2c21cbf6ae44b31716d8dac786bf
Henrik Svensson <henrik@hejare.se>
2018-12-24 15:41:01 +0100

    stops status logging after import job ends, plus some refactoring

--- wdip-be/src/config/config.js
+++ wdip-be/src/config/config.js
@@ -7,7 +7,7 @@ const defaultConfig = {
     "WDIP_DEFAULT_PARTIES": ["s", "m", "l", "kd", "v", "sd", "c", "mp"],
     "STATUS_INDEX_SQS": "status-sqs",
     "STATUS_INDEX_IPS": "status-ips",
-    "STATUS_INTERVAL_IPS_MS": 60000,
+    "STATUS_INTERVAL_IPS_MS": 1000,
     "FETCH_MOTIONS": true,
     "FETCH_PROPOSITIONS": false,
     "SQS_URL": "http://sqs:9324/queue/wdip-import"
