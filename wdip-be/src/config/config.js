90271ef0d870a8f367897ce353ed2d878394cb73
Markus Silberstein Hont <markus@silberstein.nu>
2019-01-07 14:31:45 +0100

    ignores DB credentials when running locally, deletes unsed config

--- wdip-be/src/config/config.js
+++ wdip-be/src/config/config.js
@@ -1,15 +1,13 @@
 const defaultConfig = {
-    "AWS_REGION": "eu-west-1",
-    "ES_SERVER": "elasticsearch:9200",
-    "ES_USERNAME": null,
-    "ES_PASSWORD": null,
-    "WDIP_MOTION_INDEX": "motions",
-    "WDIP_MOTION_REQUEST_LOG_INDEX": "motion-request-log",
-    "WDIP_DEFAULT_PARTIES": ["s", "m", "l", "kd", "v", "sd", "c", "mp"],
-    "STATUS_INDEX_SQS": "status-sqs",
-    "STATUS_INDEX_IPS": "status-ips",
-    "STATUS_INTERVAL_IPS_MS": 1000,
-    "SQS_URL": "http://sqs:9324/queue/wdip-import-local"
+    AWS_REGION: "eu-west-1",
+    ES_SERVER: "elasticsearch:9200",
+    SQS_URL: "http://sqs:9324/queue/wdip-import-local",
+    STATUS_INDEX_IPS: "status-ips",
+    STATUS_INDEX_SQS: "status-sqs",
+    STATUS_INTERVAL_IPS_MS: 1000,
+    WDIP_DEFAULT_PARTIES: ["s", "m", "l", "kd", "v", "sd", "c", "mp"],
+    WDIP_IS_LOCAL_ENV: false,
+    WDIP_MOTION_INDEX: "motions"
 };
 
 const config = {};
--- wdip-be/src/dbclient.ts
+++ wdip-be/src/dbclient.ts
@@ -1,16 +1,22 @@
-import AWS from "aws-sdk";
+import { Config, EnvironmentCredentials } from "aws-sdk";
 import { Client } from "elasticsearch";
 import connectionClass from "http-aws-es";
 import config from "./config/config";
+
+// No need for awsConfig if we're running locally
+let awsConfig = null;
+if (!config.WDIP_IS_LOCAL_ENV) {
+    awsConfig = new Config({
+        region: config.AWS_REGION,
+        credentials: new EnvironmentCredentials("AWS")
+    });
+}
 
 const options = {
     hosts: [config.ES_SERVER],
     log: "error",
     connectionClass,
-    awsConfig: new AWS.Config({
-        region: config.AWS_REGION,
-        credentials: new AWS.EnvironmentCredentials("AWS")
-    })
+    awsConfig
 };
 
 const dbClient = new Client(options);
