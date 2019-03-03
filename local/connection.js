f70fdc3902151a35373df4241a6181ea3dd224e4
Markus Silberstein Hont <markus@silberstein.nu>
2019-03-03 11:27:13 +0100

    removes unnecessary local/ folder

--- local/connection.js
+++
@@ -1,9 +0,0 @@
-const elasticsearch = require('elasticsearch');
-const { constants } = require('./constants');
-
-let client = new elasticsearch.Client({
-    host: constants.LOCAL_DB_PATH,
-    log: 'info'
-});
-
-module.exports = client;
